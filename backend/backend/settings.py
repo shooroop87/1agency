588671fced2ea06d503fd224b0933995974b0f8c
Andrey <shooroop87@mail.ru>
2025-12-08 11:34:22 +0300

    v2025-12-8 v3
    v2025-12-8 v3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "1ins#Zvdsecure1-1default1asdsdasdasd1$5%!#")
 
+# Debug mode
 DEBUG = False
 
 # ALLOWED HOSTS
